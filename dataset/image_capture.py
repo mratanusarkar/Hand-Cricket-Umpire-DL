b0d04480378341976575c327bca6a5eecfce010f
Atanu Sarkar <mratanusarkar@gmail.com>
2020-12-17 20:39:21 +0530

    image_capture.py v1.3
    - minor changes
    - "user input variables" section added

--- dataset/image_capture.py
+++ dataset/image_capture.py
@@ -1,6 +1,6 @@
 # Author: Atanu Sarkar
 # image capture
-# v1.2
+# v1.3
 # 15-December-2020
 
 import numpy as np
@@ -8,11 +8,17 @@ import pygame
 import cv2
 from _datetime import datetime
 
+# user input variables
+DEBUGGING_MODE = False          # make this flag True to enable debugging mode
+USE_CAMERA_NUMBER = 0           # default is 0
+FLIP_CAMERA = False             # to flip camera horizontally
+SIDE = 256                      # side length (in px) of the capture image for the dataset
+
 # initialize pygame
 pygame.init()
 
 # initialize opencv video capture
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap = cv2.VideoCapture(USE_CAMERA_NUMBER, cv2.CAP_DSHOW)
 
 # input video frame properties
 _, frame = cap.read()           # (480, 640, 3)
@@ -24,9 +30,8 @@ window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("dataset creator")
 
 # global variables
-debugging_mode = False          # make this flag True to enable debugging mode
 running = cap.isOpened()
-SIDE = 256                      # side length (in px) of the capture image
+
 x1 = int((WIDTH - SIDE) / 2)    # x1, x2, y1, y2 are the coordinates of capture square
 x2 = int((WIDTH + SIDE) / 2)
 y1 = int((HEIGHT - SIDE) / 2)
@@ -58,6 +63,9 @@ Key_7_Pressed = 0
 Q_Key_Pressed = 0
 ESC_Key_Pressed = 0
 
+if DEBUGGING_MODE:
+    print("Window Size: " + str(WIDTH) + " x " + str(HEIGHT) + "  (in px)")
+
 # main app loop begins
 while running:
 
@@ -74,59 +82,59 @@ while running:
         if event.type == pygame.KEYDOWN:
             # Esc Key down
             if event.key == pygame.K_ESCAPE:
-                if debugging_mode:
+                if DEBUGGING_MODE:
                     print("LOG: Escape Key Pressed Down")
                 ESC_Key_Pressed = 1
             # Q Key down
             if event.key == pygame.K_q:
-                if debugging_mode:
+                if DEBUGGING_MODE:
                     print("LOG: Q Key Pressed Down")
                 Q_Key_Pressed = 1
             # Num 0 Key down
             if event.key == pygame.K_0:
-                if debugging_mode:
+                if DEBUGGING_MODE:
                     print("LOG: Num 0 Key Pressed Down")
                 Key_0_Pressed = 1
                 capture_0 = True
             # Num 1 Key down
             if event.key == pygame.K_1:
-                if debugging_mode:
+                if DEBUGGING_MODE:
                     print("LOG: Num 1 Key Pressed Down")
                 Key_1_Pressed = 1
                 capture_1 = True
             # Num 2 Key down
             if event.key == pygame.K_2:
-                if debugging_mode:
+                if DEBUGGING_MODE:
                     print("LOG: Num 2 Key Pressed Down")
                 Key_2_Pressed = 1
                 capture_2 = True
             # Num 3 Key down
             if event.key == pygame.K_3:
-                if debugging_mode:
+                if DEBUGGING_MODE:
                     print("LOG: Num 3 Key Pressed Down")
                 Key_3_Pressed = 1
                 capture_3 = True
             # Num 4 Key down
             if event.key == pygame.K_4:
-                if debugging_mode:
+                if DEBUGGING_MODE:
                     print("LOG: Num 4 Key Pressed Down")
                 Key_4_Pressed = 1
                 capture_4 = True
             # Num 5 Key down
             if event.key == pygame.K_5:
-                if debugging_mode:
+                if DEBUGGING_MODE:
                     print("LOG: Num 5 Key Pressed Down")
                 Key_5_Pressed = 1
                 capture_5 = True
             # Num 6 Key down
             if event.key == pygame.K_6:
-                if debugging_mode:
+                if DEBUGGING_MODE:
                     print("LOG: Num 6 Key Pressed Down")
                 Key_6_Pressed = 1
                 capture_6 = True
             # Num 7 Key down
             if event.key == pygame.K_7:
-                if debugging_mode:
+                if DEBUGGING_MODE:
                     print("LOG: Num 7 Key Pressed Down")
                 Key_7_Pressed = 1
                 capture_7 = True
@@ -135,52 +143,52 @@ while running:
         if event.type == pygame.KEYUP:
             # Esc Key up
             if event.key == pygame.K_ESCAPE:
-                if debugging_mode:
+                if DEBUGGING_MODE:
                     print("LOG: Escape Key Released")
                 ESC_Key_Pressed = 0
             # Q Key up
             if event.key == pygame.K_q:
-                if debugging_mode:
+                if DEBUGGING_MODE:
                     print("LOG: Q Key Released")
                 Q_Key_Pressed = 0
             # Num 0 Key up
             if event.key == pygame.K_0:
-                if debugging_mode:
+                if DEBUGGING_MODE:
                     print("LOG: Num 0 Key Released")
                 Key_0_Pressed = 0
             # Num 1 Key up
             if event.key == pygame.K_1:
-                if debugging_mode:
+                if DEBUGGING_MODE:
                     print("LOG: Num 1 Key Released")
                 Key_1_Pressed = 0
             # Num 2 Key up
             if event.key == pygame.K_2:
-                if debugging_mode:
+                if DEBUGGING_MODE:
                     print("LOG: Num 2 Key Released")
                 Key_2_Pressed = 0
             # Num 3 Key up
             if event.key == pygame.K_3:
-                if debugging_mode:
+                if DEBUGGING_MODE:
                     print("LOG: Num 3 Key Released")
                 Key_3_Pressed = 0
             # Num 4 Key up
             if event.key == pygame.K_4:
-                if debugging_mode:
+                if DEBUGGING_MODE:
                     print("LOG: Num 4 Key Released")
                 Key_4_Pressed = 0
             # Num 5 Key up
             if event.key == pygame.K_5:
-                if debugging_mode:
+                if DEBUGGING_MODE:
                     print("LOG: Num 5 Key Released")
                 Key_5_Pressed = 0
             # Num 6 Key up
             if event.key == pygame.K_6:
-                if debugging_mode:
+                if DEBUGGING_MODE:
                     print("LOG: Num 6 Key Released")
                 Key_6_Pressed = 0
             # Num 7 Key up
             if event.key == pygame.K_7:
-                if debugging_mode:
+                if DEBUGGING_MODE:
                     print("LOG: Num 7 Key Released")
                 Key_7_Pressed = 0
 
@@ -266,11 +274,15 @@ while running:
         print("LOG: " + filename + " Saved to " + path + "successfully!!")
 
     # frame shape -> (480, 640, 3)
-    if debugging_mode:
+    if DEBUGGING_MODE:
         frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 5)
 
+    # flip image frame if FLIP_CAMERA
+    if FLIP_CAMERA:
+        frame = cv2.flip(frame, 1)
+
     # opencv view (for debugging)
-    if debugging_mode:
+    if DEBUGGING_MODE:
         cv2.imshow("opencv", frame)
 
     # convert to pygame surface
